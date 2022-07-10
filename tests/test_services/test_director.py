809eaed237c7ccf90a4f2e289c2679d66bc25136
jagernau <stepanenkov25@gmail.com>
2022-07-10 12:42:38 +0300

    new3

--- tests/test_services/test_director.py
+++ tests/test_services/test_director.py
@@ -1,4 +1,5 @@
 from unittest.mock import MagicMock
+
 import pytest
 
 
@@ -25,5 +26,33 @@ def director_dao():
     director_dao.update = MagicMock()
 
     return director_dao
+
+class TestDirectorService:
+    @pytest.fixture(autouse=True)
+    def director_service(self, director_dao):
+        self.director_service = DirectorService(dao=director_dao)
+
+    def test_get_one(self):
+        director = self.director_service.get_one(1)
+        assert director != None
+        assert director.id != None
+
+    def test_get_all(self):
+        directors = self.director_service.get_all()
+        assert len(directors) > 0
+    
+    def test_create(self):
+        director_d = {
+            "name": "kusturica",
+        }
+        director = self.director_service.create(director_d)
+        assert director.id != None
+
+    def test_delete(self):
+        self.director_service.delete(1)
+
+
+
+
 
 
